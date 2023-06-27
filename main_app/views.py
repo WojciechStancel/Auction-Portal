2ba5677154d3518645081688d9c5d9b4022be8ff
Uzytkownik <wojciech.stancel@gmail.com>
2023-06-27 18:36:23 +0200

    Created views for main_app

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    return render(request,
+                  'main_app/home.html')
+
+
+def about_us(request):
+    return render(request, 'main_app/about_us.html')
