661e2294fb3e7a6be1f1d7e164c0325f5f217f79
Uzytkownik <wojciech.stancel@gmail.com>
2023-06-27 22:43:12 +0200

    registered models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from product.models import Product, Category
+from order.models import Order, OrderItem
+from cart.models import ShoppingCart, ShoppingCartItem
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(ShoppingCart)
+admin.site.register(ShoppingCartItem)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(OrderItem)
