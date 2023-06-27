2c195366c738a4fd982ec5fad5c1d6cd74c06b7c
Uzytkownik <wojciech.stancel@gmail.com>
2023-06-27 18:22:56 +0200

    1. Created register, login and logout views, 2. Created header.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,13 @@
+from django.urls import path
+
+from accounts import views
+
 app_name = 'accounts'
 
 urlpatterns = [
+
+    path('register/', views.register_request, name='register'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout, get_user_model
+from django.contrib import messages
+from .forms import CreateUserForm
 
-# Create your views here.
+User = get_user_model()
+
+
+def register_request(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, f'Account was created for {user}')
+            return redirect('accounts:login')
+
+    return render(request, 'accounts/register.html', context={'register_form': form})
+
+
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('product:auctions')
+        else:
+            messages.info(request, 'Wrong username or password')
+
+    return render(request, 'accounts/login.html')
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('main_app:home')
